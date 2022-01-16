6253d9d94f6eae8b91d70b248c644aae39384547
Seong-Gook Lim <sklim0932@gmail.com>
2022-01-17 00:15:39 +0900

    템플릿 메소드 패턴 적용

---
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,51 @@
+package hello.advanced.trace.template;
+
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+    }
+
+    private void logic1() {
+
+    }
+
+    /**
+     * 템플릿 메소드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        };
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        };
+        template2.execute();
+    }
+}
